d45a7719efcae45bd7b17c3aa1a2f6fcb6f629a8
Orange Lee <33177912+UnnamedOrange@users.noreply.github.com>
2018-03-06 22:37:58 +0800

    学习
    AC 自动机 - Trie 图
    整理笔记

--- Source Code/AC 自动机/template.cpp
+++ Source Code/AC 自动机/template.cpp
@@ -2,6 +2,9 @@
 #include <cstdlib>
 #include <cmath>
 #include <cstring>
+#include <cctype>
+#include <climits>
+#include <cassert>
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -13,22 +16,25 @@
 #include <set>
 #include <bitset>
 #include <list>
-typedef int INT;
+#include <functional>
+typedef long long LL;
+typedef unsigned long long ULL;
 using std::cin;
 using std::cout;
 using std::endl;
-INT readIn()
+typedef int INT_PUT;
+INT_PUT readIn()
 {
-	INT a = 0;
+	INT_PUT a = 0;
 	bool minus = false;
 	char ch = getchar();
-	while (!(ch == '-' || (ch >= '0' && ch <= '9'))) ch = getchar();
+	while (!(ch == '-' || std::isdigit(ch))) ch = getchar();
 	if (ch == '-')
 	{
 		minus = true;
 		ch = getchar();
 	}
-	while (ch >= '0' && ch <= '9')
+	while (std::isdigit(ch))
 	{
 		a = a * 10 + (ch - '0');
 		ch = getchar();
@@ -36,10 +42,10 @@ INT readIn()
 	if (minus) a = -a;
 	return a;
 }
-void printOut(INT x)
+void printOut(INT_PUT x)
 {
 	char buffer[20];
-	INT length = 0;
+	int length = 0;
 	if (x < 0)
 	{
 		putchar('-');
@@ -57,111 +63,141 @@ void printOut(INT x)
 	putchar('\n');
 }
 
-const INT maxn = INT(1e6) + 5;
-INT n;
-
-class ACAutomation
+struct ACAutomation
 {
-	static const INT alphabet = 26;
-	inline static INT code(char ch)
+	static const int alphabet = 26;
+	static inline int code(char ch)
 	{
 		return ch - 'a';
 	}
-
-	INT clock_;
 	struct Node
 	{
 		Node* c[alphabet];
 		Node* fail;
 		Node* suffix;
-
-		INT count;
-		INT dfn;
-		Node() : c(), fail(), suffix(), count(), dfn(-1) {}
+		int cnt;
+		Node() : c(), fail(), suffix(), cnt() {}
 	};
-	Node* root;
+	struct Pool
+	{
+		std::stack<Node*> p;
+		std::stack<Node*> used;
+		Node* alloc()
+		{
+			Node* ret;
+			if (p.empty()) ret = new Node;
+			else
+			{
+				ret = p.top();
+				p.pop();
+				used.push(ret);
+				new(ret) Node;
+			}
+			return ret;
+		}
+		void reset()
+		{
+			while (!used.empty())
+			{
+				p.push(used.top());
+				used.pop();
+			}
+		}
+	};
+	static Pool pool;
 
-public:
+	Node* root;
 	ACAutomation()
 	{
-		root = new Node;
+		isInitialized = false;
+		root = pool.alloc();
+	}
+	void reset()
+	{
+		pool.reset(); // 需要改造内存池
+		new(this) ACAutomation;
 	}
 
-	void insert(const char* T)
+	bool isInitialized;
+
+	void insert(const char* str)
 	{
+		if (isInitialized) throw;
 		Node* cnt = root;
-		for (; *T != '\0'; T++)
+		for (; *str != '\0'; str++)
 		{
-			Node* &next = cnt->c[code(*T)];
-			if (!next)
-				next = new Node;
-			cnt = next;
+			Node* &p = cnt->c[code(*str)];
+			if (!p) p = pool.alloc();
+			cnt = p;
 		}
-		cnt->count++;
+		cnt->cnt++;
 	}
-	void init()
+	void initialize()
 	{
-		BFS();
-	}
-	void BFS()
-	{
-		static Node* q[maxn];
-		INT head = 0, tail = 0;
-		q[tail++] = root;
-		while (head != tail)
+		if (isInitialized) throw;
+		isInitialized = true;
+		std::queue<Node*> q;
+		q.push(root);
+		while (!q.empty())
 		{
-			Node* p = q[head++];
+			Node* cnt = q.front();
+			q.pop();
 			for (int i = 0; i < alphabet; i++)
 			{
-				if (Node* &p2 = p->c[i])
+				Node* &to = cnt->c[i];
+				Node* t = cnt == root ? root : cnt->fail->c[i]; // 由于是递推，所以 cnt->fail->c[i] 一定是已经算了的
+				if (!to)
+					to = t; // 没有的话直接 = cnt->fail->c[i] 构建 Trie 图
+				else
 				{
-					q[tail++] = p2;
-
-					Node* pre = p->fail;
-					while (pre && !pre->c[i])
-						pre = pre->fail;
-					p2->fail = pre ? pre->c[i] : root;
-					p2->suffix = p2->fail->count ? p2->fail : p2->suffix;
+					to->fail = t; // 有的话计算出 Fail 指针
+					to->suffix = to->fail->cnt ? to->fail : to->fail->suffix; //记住算出 suffix
+					q.push(to);
 				}
 			}
 		}
 	}
-	INT match(const char* S)
+	int match(const char* str)
 	{
-		clock_++;
-		INT ret = 0;
+		if (!isInitialized) throw;
+		int ret = 0;
 		Node* cnt = root;
-		for (; *S != '\0'; S++)
+		for (; *str != '\0'; str++)
 		{
-			INT ch = code(*S);
-			while (cnt && !cnt->c[ch]) cnt = cnt->fail;
-			cnt = cnt ? cnt->c[ch] : root;
-			for (Node* t = cnt->count ? cnt : cnt->suffix; t; t = t->suffix)
+			int c = code(*str);
+			cnt = cnt->c[c]; // 直接走
+			for (Node* t = cnt; t; t = t->suffix)
 			{
-				if (t->dfn != clock_)
-				{
-					t->dfn = clock_;
-					ret += t->count;
-				}
+				ret += t->cnt;
+				t->cnt = 0;
 			}
 		}
 		return ret;
 	}
 };
+ACAutomation::Pool ACAutomation::pool;
 
-char buffer[maxn];
+int n;
+ACAutomation ac;
+
 void run()
 {
-	n = readIn();
-	ACAutomation ac;
-	for (int i = 1; i <= n; i++)
+	int T = readIn();
+	while (T--)
 	{
+		n = readIn();
+		ac.reset();
+		for (int i = 1; i <= n; i++)
+		{
+			static char buffer[55];
+			scanf("%s", buffer);
+			ac.insert(buffer);
+		}
+		ac.initialize();
+		static char buffer[1000005];
 		scanf("%s", buffer);
-		ac.insert(buffer);
+		printOut(ac.match(buffer));
 	}
-	ac.init();
-	scanf("%s", buffer);
-	printOut(ac.match(buffer));
 }
 
 int main()
