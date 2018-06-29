c8b79c0aab67d190196fdb33181e335106a5c808
Orange Lee <lycheng1215@sina.com>
2018-06-29 14:39:50 +0800

    2018-6-28

--- Camp/2018-6-28/source/matrix.cpp
+++ Camp/2018-6-28/source/matrix.cpp
@@ -18,7 +18,6 @@
 #include <bitset>
 #include <list>
 #include <functional>
-#include <array>
 using LL = long long;
 using ULL = unsigned long long;
 using std::cin;
@@ -80,7 +79,10 @@ struct Status
 	{
 		// 相邻的不能是黑块
 		for (int i = 1; i < n; i++)
-			if (!(S & (1 << i)) && (!(S & (1 << (i - 1))) || !val[i]))
+			if (!(S & (1 << i)) && !(S & (1 << (i - 1))))
+				return false;
+		for (int i = 0; i < n; i++)
+			if (!(S & (1 << i)) && !val[i])
 				return false;
 
 		// 转移后上面的不能不连通
@@ -104,10 +106,11 @@ struct Status
 				transTo.val[i] = (i && transTo.val[i - 1]) ? transTo.val[i - 1] : ++nBlock;
 			else
 				transTo.val[i] = 0;
+
+		// 以连续的一块为单位进行操作
 		for (int i = 0; i < n; i++)
 			if (val[i] && transTo.val[i])
 				ds.unite(transTo.val[i] + n, val[i]);
-
 		for (int i = 0; i < n; i++)
 			if (transTo.val[i])
 				transTo.val[i] = ds.find(transTo.val[i] + n);
@@ -120,6 +123,12 @@ struct Status
 				transTo.val[i] = newIdx[transTo.val[i]] = ++nBlock;
 		return true;
 	}
+	int bEnd{ -1 };
+	bool isEnd()
+	{
+		if (~bEnd) return bEnd;
+		return bEnd = (*std::max_element(val, val + n) == 1);
+	}
 };
 std::map<Status, int> mapSI;
 const int maxStatus = 3500;
@@ -130,7 +139,8 @@ void search(int depth, int nBlock)
 	static Status temp;
 	if (depth >= n)
 	{
-		mapSI[temp] = mapSI.size() + 1;
+		int t = mapSI.size() + 1;
+		mapSI[temp] = t;
 		status[mapSI.size()] = temp;
 	}
 	else
@@ -157,12 +167,89 @@ void initTransfer()
 		{
 			Status to;
 			if (status[i].makeTransfer(S, to))
-			{
-				assert(mapSI.count(to));
 				transfer[i].push_back(mapSI[to]);
-			}
 		}
 }
+
+#define RunInstance(x) delete new x
+struct brute
+{
+	struct Matrix
+	{
+		static const int maxsize = 260; // n = 8 时，有 259 种状态
+		int size;
+		int c[maxsize][maxsize];
+		int* operator[](int x) { return c[x]; }
+		const int* operator[](int x) const { return c[x]; }
+		explicit Matrix(int size) : size(size), c() {}
+		Matrix(int size, bool) : Matrix(size)
+		{
+			for (int i = 1; i <= size; i++)
+				c[i][i] = 1;
+		}
+		Matrix operator*(const Matrix& b) const
+		{
+			register int t = size;
+			Matrix ret(t);
+			for (int i = 1; i <= t; i++)
+				for (int k = 1; k <= t; k++) if (c[i][k])
+					for (int j = 1; j <= t; j++)
+						ret[i][j] = (ret[i][j] + (LL)c[i][k] * b[k][j]) % mod;
+			return ret;
+		}
+		Matrix operator^(int y) const
+		{
+			Matrix ret(size, true);
+			Matrix x(*this);
+			while (y)
+			{
+				if (y & 1) ret = ret * x;
+				x = x * x;
+				y >>= 1;
+			}
+			return ret;
+		}
+	};
+	Matrix base;
+
+	brute() : base(mapSI.size())
+	{
+		for (int i = 1; i <= mapSI.size(); i++)
+			for (int j = 0; j < transfer[i].size(); j++)
+				base[i][transfer[i][j]]++;
+		for (int i = 1; i <= base.size; i++)
+			for (int j = 1; j <= base.size; j++)
+				base[i][j] %= mod;
+
+		int ans = 0;
+		base = base ^ (m - 1);
+		int U = 1 << n;
+		for (int S = 0; S < U; S++)
+		{
+			bool bOk = true;
+			for (int i = 1; i < n; i++)
+				if (!(S & (1 << i)) && !(S & (1 << (i - 1))))
+				{
+					bOk = false;
+					break;
+				}
+			if (!bOk) continue;
+			Status start;
+			int nBlock = 0;
+			for (int i = 0; i < n; i++)
+				if (S & (1 << i))
+					start.val[i] = (i && start.val[i - 1]) ? start.val[i - 1] : ++nBlock;
+				else
+					start.val[i] = 0;
+
+			int id = mapSI[start];
+			for (int i = 1; i <= mapSI.size(); i++)
+				if (status[i].isEnd())
+					ans = (ans + base[id][i]) % mod;
+		}
+		printOut(ans);
+	}
+};
 
 void run()
 {
@@ -173,10 +260,8 @@ void run()
 	search(0, 0);
 	initTransfer();
 
-	int count = 0;
-	for (int i = 1; i <= mapSI.size(); i++)
-		count += transfer[i].size();
-	cout << count << endl;
+	if (n <= 8)
+		RunInstance(brute);
 }
 
 int main()
